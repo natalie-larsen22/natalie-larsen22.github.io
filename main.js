fdc35549ae79f4a072f17b9d78687f52d5757ea3
natalie-larsen22 <157241140+natalie-larsen22@users.noreply.github.com>
2024-03-02 10:26:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+var greenButton = document.getElementById("green");
+
+console.log(greenButton);
+
+greenButton.addEventListener('click', banana);
+
+function banana() {
+    alert("Bananas, In Pajamas");
+}
+
